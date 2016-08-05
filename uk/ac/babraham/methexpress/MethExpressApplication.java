354619072d785f8c7a7b14de2dfcb362d541d11b
s-andrews <simon.andrews@babraham.ac.uk>
2016-08-05 11:56:58 +0100

    Working on the data structure.

--- uk/ac/babraham/methexpress/MethExpressApplication.java
+++ uk/ac/babraham/methexpress/MethExpressApplication.java
@@ -1,5 +1,31 @@
 package uk.ac.babraham.methexpress;
 
+import uk.ac.babraham.methexpress.preferences.Preferences;
+import uk.ac.babraham.methexpress.preferences.PreferencesParsingException;
+
 public class MethExpressApplication {
 
+	private Preferences prefs;
+
+	public MethExpressApplication (String [] args) {
+		
+		try {
+			prefs = new Preferences(args);
+		} 
+		catch (PreferencesParsingException e) {
+			System.err.println("Failed to parse command line: "+e.getLocalizedMessage());
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		
+
+	}
+	
+	
+	public static void main (String [] args) {
+		
+		new MethExpressApplication(args);
+	}
+	
 }
